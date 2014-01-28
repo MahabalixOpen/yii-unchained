3740faf7e33c7ba289ddbd0c911ee7c3268a2407
Mahabalix Software <officedesk@mahabalix.com>
2014-01-28 22:02:18 +0400

    Added Rights
    Added Rights and fixed login problem by changing CWebuser to WebUser in
    rights(WebUser is from Yii-user)

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ yii-unchained
 =============
 
 Extended versions of the Yii framework to include popular extensions such as yii-user,rights and so on.
+
+
+
+INSTALLATION
+------------
+1. Migrate Yii Site and SQL database
+
+
+Issues
+------
+1. Fix caps table names for Rights for compatibility on linux servers.
+
+
+Changes
+-------
+
+2.Added Rights and changed Rights-> "class RWebUser extends CWebUser" -> "class RWebUser extends WebUser". This fixes login error.(WebUser was introduced by Yii-user)
+1.Added Yii-User
--- yii-unchained/testdrive/protected/config/main.php
+++ yii-unchained/testdrive/protected/config/main.php
@@ -18,6 +18,8 @@ return array(
 		'application.components.*',
 		 'application.modules.user.models.*',
         'application.modules.user.components.*',
+		'application.modules.rights.*',
+		'application.modules.rights.components.*',
 	),
 
 	'modules'=>array(
@@ -62,16 +64,36 @@ return array(
             'returnLogoutUrl' => array('/user/login'),
 		),
 		
+		'rights'=>array(
+		'superuserName'=>'Admin', // Name of the role with super user privileges.
+ 'authenticatedName'=>'Authenticated', // Name of the authenticated user role. 
+ 'userIdColumn'=>'id', // Name of the user id column in the database.
+ 'userNameColumn'=>'username', // Name of the user name column in the database. 
+ 'enableBizRule'=>true, // Whether to enable authorization item business rules.
+ 'enableBizRuleData'=>false, // Whether to enable data for business rules.
+ 'displayDescription'=>true, // Whether to use item description instead of name.
+ 'flashSuccessKey'=>'RightsSuccess', // Key to use for setting success flash messages.
+ 'flashErrorKey'=>'RightsError', // Key to use for setting error flash messages.
+ 'baseUrl'=>'/rights', // Base URL for Rights. Change if module is nested. 
+ 'layout'=>'rights.views.layouts.main', // Layout to use for displaying Rights.
+ 'appLayout'=>'application.views.layouts.main', // Application layout.
+ 'cssFile'=>'rights.css',  // Style sheet file to use for Rights. 
+ 'install'=>false, // Whether to enable installer.
+ 'debug'=>false, // Whether to enable debug mode.
+		),
+		
 	),
 
 	// application components
 	'components'=>array(
 		'user'=>array(
             // enable cookie-based authentication
-            'class' => 'WebUser',
+            'class' => 'RWebUser',
             'allowAutoLogin'=>true,
             'loginUrl' => array('/user/login'),
         ),
+		
+		'authManager'=>array('class' => 'RDbAuthManager',),
 		// uncomment the following to enable URLs in path-format
 		
 		'urlManager'=>array(
